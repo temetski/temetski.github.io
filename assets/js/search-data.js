156c374c0824d69341242b9e0b86deb9f8543ed8
temetski <damian.dailisan@gmail.com>
2025-03-06 16:55:21 +0000

    Deploying to gh-pages from @ temetski/temetski.github.io@0ea17267bb129ae589648ecc9126f36833dbbeaa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "A PDF version of my CV is also downloadable here.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
